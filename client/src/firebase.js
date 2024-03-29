24995b3dbe0c3b9df6c7cc5c8589abd64661e051
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-03-29 18:16:51 +0530

    frontend of app is created

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoFVv_7PqPEM2JRb-A46igduY_HImHjqM",
+  authDomain: "mern-estate-375af.firebaseapp.com",
+  projectId: "mern-estate-375af",
+  storageBucket: "mern-estate-375af.appspot.com",
+  messagingSenderId: "927869653888",
+  appId: "1:927869653888:web:1bc0db1d0f1db18b51a765"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
